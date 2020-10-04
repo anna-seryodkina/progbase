2efe326e80dd092c79fe1322bf2ebc75ee76a9a0
anna-serodkina <a.serodkina@gmail.com>
2020-10-04 22:24:33 +0300

    task1 done

---
+++ projects/tasks/task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+
+class Program
+{
+    static void Main()
+    {
+        WriteLine("Hello! My name is Anya Seryodkina :)"); // -- this is line number 7 --
+        WriteLine("I am from KP-03");
+        /*
+        ^-^
+        
+        block
+            with 
+                comments
+        
+        ^-^
+        */
+    }
+}
