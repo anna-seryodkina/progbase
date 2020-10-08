e2a7fc401f4952f19280980b5fbcbc2551becd43
anna-serodkina <a.serodkina@gmail.com>
2020-10-08 20:01:43 +0300

    task 4

--- projects/tasks/task_4/Program.cs
+++ projects/tasks/task_4/Program.cs
@@ -1,10 +1,32 @@
 ﻿using static System.Console;
+using static System.Math;
 
 
 class Program
 {
     static void Main()
     {
-        WriteLine("Hello World!");
+        // 1
+        
+        WriteLine("1.Enter x:");
+        double x1 = double.Parse(ReadLine());
+        x1 = Pow (x1,2) - Sin (x1);
+        WriteLine(x1); 
+
+        // 2
+        
+        WriteLine("2.Enter x:");
+        double x2 = double.Parse(ReadLine());
+        x2 = Sqrt(Pow(Cos(x2), 2) + Abs(x2));
+        x2 = Ceiling(x2);
+        WriteLine(x2);
+        
+        // 3
+        
+        WriteLine("3.Enter x:");
+        double x3 = double.Parse(ReadLine());
+        x3 = (1 / (x3 + 3) ) - ( (Pow (x3,2) + 50) / 2 );
+        WriteLine(x3);
+
     }
 }
