62e16bbb92861b1f40de80061cb458d0a6a786f4
anna-serodkina <a.serodkina@gmail.com>
2020-11-12 17:30:35 +0200

    changes

---
+++ projects/tasks/task_5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("number 1:");
+            int num1= int.Parse(ReadLine());
+            Console.WriteLine("number 2:");
+            int num2= int.Parse(ReadLine());
+            Console.WriteLine("number 3:");
+            int num3= int.Parse(ReadLine());
+
+            bool check1 = (num1+num2>num3);
+            bool check2 = (num2+num3>num1);
+            bool check3 = (num3+num1>num2);
+            if(check1 && check2 && check3){
+
+                if(num1==num2 && num1==num3){
+
+                    Console.WriteLine("equilateral");
+
+                } else if(num1==num2 || num1==num3 || num2 == num3){
+
+                    Console.WriteLine("isosceles");
+
+                } else {
+
+                    Console.WriteLine("arbitrary");
+                }
+
+            }else {
+                Console.WriteLine("does not exist");
+            }
+            */
+        }
+    }
